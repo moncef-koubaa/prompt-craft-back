a22e94e00c74afa118991aa213e810a244c6e43f
Hsan003 <hsan.ellouze@insat.ucar.tn>
2025-05-30 20:01:05 +0100

    feat: mutex creation

--- src/auction/auction.service.ts
+++ src/auction/auction.service.ts
@@ -12,163 +12,184 @@ import { User } from 'src/user/entities/user.entity';
 import { Cron } from '@nestjs/schedule';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { UserService } from 'src/user/user.service';
+import { PriorityMutex } from './priority-mutex';
 
 @Injectable()
 export class AuctionService {
-  constructor(
-    @InjectRepository(Auction)
-    private auctionRepo: Repository<Auction>,
-    @InjectRepository(Bid)
-    private bidRepo: Repository<Bid>,
-    @InjectRepository(JoinAuction)
-    private joinRepo: Repository<JoinAuction>,
-    private readonly eventEmitter: EventEmitter2,
-    private readonly userService: UserService
-  ) {}
+    constructor(
+        @InjectRepository(Auction)
+        private auctionRepo: Repository<Auction>,
+        @InjectRepository(Bid)
+        private bidRepo: Repository<Bid>,
+        @InjectRepository(JoinAuction)
+        private joinRepo: Repository<JoinAuction>,
+        private readonly eventEmitter: EventEmitter2,
+        private readonly userService: UserService
+    ) { }
 
-  async createAuction(dto: CreateAuctionDto) {
-    // TODO: make with connected user check for onership and if already on auction change nft status
-    const auction = this.auctionRepo.create(dto);
-    return await this.auctionRepo.save(auction);
-  }
 
-  async getAuction(auctionId: number) {
-    log('Fetching auction with ID:', auctionId);
-    const auction = await this.auctionRepo.findOne({
-      where: { id: auctionId },
-      relations: ['bids', 'participants'],
-    });
+    private auctionLocks: Map<number, PriorityMutex> = new Map();
 
-    return auction;
-  }
-
-  async joinAuction(auctionId: number, user: User) {
-    const auction = await this.auctionRepo.findOneBy({ id: auctionId });
-    if (!auction) throw new BadRequestException('Auction not found');
-
-    if (auction.ownerId === user.id) {
-      throw new BadRequestException('You cannot join your own auction');
+    private getLockForAuction(auctionId: number): PriorityMutex {
+        if (!this.auctionLocks.has(auctionId)) {
+            this.auctionLocks.set(auctionId, new PriorityMutex());
+        }
+        return this.auctionLocks.get(auctionId)!;
     }
 
-    const existingJoin = await this.joinRepo.findOne({
-      where: {
-        user: { id: user.id },
-        auction: { id: auctionId },
-      },
-    });
-    if (existingJoin) {
-      throw new BadRequestException('Already joined the auction');
+    async createAuction(dto: CreateAuctionDto) {
+        // TODO: make with connected user check for onership and if already on auction change nft status
+        const auction = this.auctionRepo.create(dto);
+        return await this.auctionRepo.save(auction);
     }
 
-    const join = this.joinRepo.create({
-      user: user,
-      auction,
-    });
+    async getAuction(auctionId: number) {
+        log('Fetching auction with ID:', auctionId);
+        const auction = await this.auctionRepo.findOne({
+            where: { id: auctionId },
+            relations: ['bids', 'participants'],
+        });
 
-    return await this.joinRepo.save(join);
-  }
-
-  async isParticipant(auctionId: number, user: User): Promise<boolean> {
-    const auction = await this.auctionRepo
-      .createQueryBuilder('auction')
-      .leftJoinAndSelect('auction.participants', 'participants')
-      .leftJoinAndSelect('participants.user', 'user')
-      .where('auction.id = :auctionId', { auctionId })
-      .getOne();
-    if (!auction) return false;
-
-    log('praticipant:', auction.participants);
-    const participant = auction.participants.find(
-      (participant) => participant.user.id === user.id
-    );
-    if (!participant) return false;
-    return true;
-  }
-
-  async getMyAuctions(userId: number) {
-    const auctions = await this.auctionRepo.find({
-      where: { ownerId: userId },
-      relations: ['bids', 'participants'],
-    });
-    return auctions;
-  }
-
-  async placeBid(dto: PlaceBidDto, bidderId: number) {
-    let auction = await this.auctionRepo.findOneBy({ id: dto.auctionId });
-    if (!auction) throw new WsException('Auction not found');
-    if (auction.isEnded) throw new WsException('Auction has ended');
-
-    const userBalance = await this.userService.getBalance(bidderId);
-    if (userBalance < dto.amount) {
-      throw new WsException('Insufficient funds');
+        return auction;
     }
 
-    if (dto.amount < auction.maxBidAmount) {
-      throw new WsException('Bid amount is less than current highest bid');
+    async joinAuction(auctionId: number, user: User) {
+        const auction = await this.auctionRepo.findOneBy({ id: auctionId });
+        if (!auction) throw new BadRequestException('Auction not found');
+
+        if (auction.ownerId === user.id) {
+            throw new BadRequestException('You cannot join your own auction');
+        }
+
+        const existingJoin = await this.joinRepo.findOne({
+            where: {
+                user: { id: user.id },
+                auction: { id: auctionId },
+            },
+        });
+        if (existingJoin) {
+            throw new BadRequestException('Already joined the auction');
+        }
+
+        const join = this.joinRepo.create({
+            user: user,
+            auction,
+        });
+
+        return await this.joinRepo.save(join);
     }
 
-    auction.winnerId = bidderId;
-    auction.maxBidAmount = dto.amount;
-    this.auctionRepo.save(auction);
+    async isParticipant(auctionId: number, user: User): Promise<boolean> {
+        const auction = await this.auctionRepo
+            .createQueryBuilder('auction')
+            .leftJoinAndSelect('auction.participants', 'participants')
+            .leftJoinAndSelect('participants.user', 'user')
+            .where('auction.id = :auctionId', { auctionId })
+            .getOne();
+        if (!auction) return false;
 
-    this.userService.freazeBidAmount(auction, bidderId, dto.amount);
-    this.userService.unfreezeBidAmount(auction);
-
-    const bid = this.bidRepo.create({
-      auction,
-      bidderId,
-      amount: dto.amount,
-    });
-
-    return await this.bidRepo.save(bid);
-  }
-
-  async endAuction(auction: Auction) {
-    auction.isEnded = true;
-    await this.auctionRepo.save(auction);
-
-    const highestBid = await this.bidRepo
-      .createQueryBuilder('bid')
-      .where('bid.auctionId = :auctionId', { auctionId: auction.id })
-      .orderBy('bid.amount', 'DESC')
-      .getOne();
-
-    if (highestBid) {
-      auction.winnerId = highestBid.bidderId;
-      await this.auctionRepo.save(auction);
-
-      this.eventEmitter.emit('auction.ended', {
-        auctionId: auction.id,
-        winnerId: highestBid.bidderId,
-        amount: highestBid.amount,
-      });
-
-      // transfer NFT ownership
-
-      // transfer money to the owner
+        log('praticipant:', auction.participants);
+        const participant = auction.participants.find(
+            (participant) => participant.user.id === user.id
+        );
+        if (!participant) return false;
+        return true;
     }
-  }
 
-  @Cron('*/5 * * * *')
-  async scheduleAuctionEnd() {
-    const now = new Date();
-    const in5Minutes = new Date(now.getTime() + 5 * 60 * 1000);
-    const auctions = await this.auctionRepo.find({
-      where: {
-        isEnded: false,
-        endTime: LessThanOrEqual(in5Minutes),
-      },
-    });
-
-    for (const auction of auctions) {
-      const timeLeft = auction.endTime.getTime() - now.getTime();
-      if (timeLeft > 0) {
-        setTimeout(async () => {
-          this.endAuction(auction);
-        }, timeLeft);
-      } else {
-        this.endAuction(auction);
-      }
+    async getMyAuctions(userId: number) {
+        const auctions = await this.auctionRepo.find({
+            where: { ownerId: userId },
+            relations: ['bids', 'participants'],
+        });
+        return auctions;
     }
-  }
+
+    async placeBid(dto: PlaceBidDto, bidderId: number) {
+        const lock = this.getLockForAuction(dto.auctionId);
+        await lock.runExclusive(async () => {
+            let auction = await this.auctionRepo.findOneBy({ id: dto.auctionId });
+            if (!auction) throw new WsException('Auction not found');
+            if (auction.isEnded) throw new WsException('Auction has ended');
+
+            const userBalance = await this.userService.getBalance(bidderId);
+            if (userBalance < dto.amount) {
+                throw new WsException('Insufficient funds');
+            }
+
+            if (dto.amount < auction.maxBidAmount) {
+                throw new WsException('Bid amount is less than current highest bid');
+            }
+
+            auction.winnerId = bidderId;
+            auction.maxBidAmount = dto.amount;
+            this.auctionRepo.save(auction);
+
+            this.userService.freazeBidAmount(auction, bidderId, dto.amount);
+            this.userService.unfreezeBidAmount(auction);
+
+            const bid = this.bidRepo.create({
+                auction,
+                bidderId,
+                amount: dto.amount,
+            });
+
+            return await this.bidRepo.save(bid);
+        }, 1);
+    }
+
+
+    async endAuction(auction: Auction) {
+        const lock = this.getLockForAuction(auction.id);
+        await lock.runExclusive(async () => {
+
+            auction.isEnded = true;
+            await this.auctionRepo.save(auction);
+
+            const highestBid = await this.bidRepo
+                .createQueryBuilder('bid')
+                .where('bid.auctionId = :auctionId', { auctionId: auction.id })
+                .orderBy('bid.amount', 'DESC')
+                .getOne();
+
+            if (highestBid) {
+                auction.winnerId = highestBid.bidderId;
+                await this.auctionRepo.save(auction);
+
+                this.eventEmitter.emit('auction.ended', {
+                    auctionId: auction.id,
+                    winnerId: highestBid.bidderId,
+                    amount: highestBid.amount,
+                });
+
+                // transfer NFT ownership
+
+                // transfer money to the owner
+            }
+        }, 1000);
+    }
+
+
+    @Cron('*/5 * * * *')
+    async scheduleAuctionEnd() {
+        const now = new Date();
+        const in5Minutes = new Date(now.getTime() + 5 * 60 * 1000);
+        const auctions = await this.auctionRepo.find({
+            where: {
+                isEnded: false,
+                endTime: LessThanOrEqual(in5Minutes),
+            },
+        });
+
+        for (const auction of auctions) {
+            const timeLeft = auction.endTime.getTime() - now.getTime();
+            if (timeLeft > 0) {
+                setTimeout(async () => {
+                    this.endAuction(auction);
+                }, timeLeft);
+            } else {
+                this.endAuction(auction);
+            }
+        }
+
+    }
 }
